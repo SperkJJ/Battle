ff041a7fd4aa3df857d6b504a5df1515f1909a2f
杰 <SperkJJ@163.com>
2019-04-09 20:46:03 +0800

    1

--- test.cpp
+++ test.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+using namespace std;
 
+int count2index(int temp)
+{
+    int ret = 0 ;
+    while((temp % 2) != 1)
+    {
+        ret++;
+        temp = temp / 2;
+    }
+    return ret;
+}
+
+int mian()
+{
+    int n = 0;
+    int m[100000] = {0};
+    int temp = 0;
+    cin >> n;
+  
+    for(int i = 0; i < n ; i++)
+    {
+      cin >> temp;
+      m[i] = count2index(temp);
+    }
+  
+    
+  
+    return 0;
+}
