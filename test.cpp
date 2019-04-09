7901234ddf7a970eadc3dc6a879dcf5289822fab
杰 <SperkJJ@163.com>
2019-04-09 20:52:32 +0800

    2

--- test.cpp
+++ test.cpp
@@ -18,13 +18,26 @@ int mian()
     int m[100000] = {0};
     int temp = 0;
     cin >> n;
-  
+    
+    // 计算指数集合
     for(int i = 0; i < n ; i++)
     {
       cin >> temp;
       m[i] = count2index(temp);
     }
-  
+    
+    // 排序指数集
+    for(int i = 1; i < n ; i++)
+    {
+        for(int j = 0; j < n - i - 1; j++)
+        {
+            if(m[j] > m[j+1])
+            {
+                temp = m[j]
+            
+            }
+        }
+    }
     
   
     return 0;
